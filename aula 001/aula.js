0089890345af3d3df856bd3e45bce3d0a7fbccd9
Thomas <thmas5952@gmail.com>
2022-05-16 00:35:51 -0300

    video 001

---
+++ aula 001/aula.js
@@ -0,0 +1,39 @@
+// Para criar váriaveis
+/* 
+Tipos de dados:
+number = numeros e tal
+string = textos
+boolean = true ou false
+null = variáveis sem valor
+undefined = valor indefinido
+
+------------------------------
+object = uma variável com variáveis dentro, usa-se {}
+
+array = um array é uma lista de valores ou objetos. usa-se [] 
+
+*/
+
+var x = 1;
+var teste = "teste";
+var nulo = null;
+//---------------------------------------------------
+x = {}
+
+var pessoa = {
+    altura: 1.87,
+    peso: 90
+};
+
+/*
+para acessar usamos variavel.(propriedade)
+*/
+pessoa.altura
+
+// array
+var numeros = [];
+
+var numeros2 = [1,23,5,6]
+
+
+
