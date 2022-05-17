18b5b141fdef4590ab188f72e4a36597f466227b
Thomas <thmas5952@gmail.com>
2022-05-17 10:51:07 -0300

    aula 001 - video 002 - update

---
+++ aula 001/video_002.js
@@ -0,0 +1,24 @@
+// operadores aritméticos, basicamente as contas matemátiacas
+// * = multiplicação
+// / = divisão
+// - = subtração
+// + = adição
+
+var x = 2
+var l = 3
+
+var res = (x * l) - l/x+1
+
+// também existe a notação -- ou ++
+
+var soma = 20
+soma++ //soma vai ser 21, pq adcionou 1 nela
+soma-- // soma vai voltar a ser 20, pois subtraiu 1
+
+
+// também é possível abreviar eles, assim ó
+
+soma += 2; // vai somar 2 no valor de soma
+soma *= 2 //vai multiplicar soma por 2
+
+// e isso vale para todas as operações
