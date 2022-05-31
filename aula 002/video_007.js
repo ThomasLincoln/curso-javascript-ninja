b8d1f7c13d61d393d3ec0a4690798c2f367b9180
Thomas <thmas5952@gmail.com>
2022-05-31 00:44:57 -0300

    aula 002 - video 007

---
+++ aula 002/video_007.js
@@ -0,0 +1,20 @@
+// é basicamente um buff no if, com eles podemos fazer mais de uma verificação, por exemplo o && q é and, com eles podemos combinar comparações
+
+if (x ==2 && y == 3){}
+
+// nesse caso ele retorna true se o x = 2 e y = 3
+
+// OPERADOR OR, o or busca saber pelo menos um valor é verdadeiro
+
+if (x == 2 || y == 3){
+}
+
+// se o x for 2 ou o y for 3 retorna true, se ambos forem tbm retorna true, mas se nenhum for, ele retorna false
+
+
+// NOT, o not ele inverte o valor, por exemplo, se o retorno for true, ele retorna false, e vice versa
+
+if (x !== 3){
+
+}
+// se o x é diferente de 3 ele retorna true, se for igual ele retorna true
